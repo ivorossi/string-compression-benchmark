6454257645caaac287cbfb3b91131ffd36551c50
ivo <rossiivomartin@gmail.com>
2023-03-09 18:44:53 -0300

    error de compressor

--- src/main/java/com/brightsector/string_compression_benchmark_ivo/algorithm/LZ4BlockCompressionAlgorithm.java
+++ src/main/java/com/brightsector/string_compression_benchmark_ivo/algorithm/LZ4BlockCompressionAlgorithm.java
@@ -6,15 +6,15 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 
-import org.apache.commons.compress.compressors.lzma.LZMACompressorOutputStream;
 
 import net.jpountz.lz4.LZ4BlockInputStream;
+import net.jpountz.lz4.LZ4BlockOutputStream;
 
 public class LZ4BlockCompressionAlgorithm extends CompressionAlgorithm {
 
 	@Override
 	public OutputStream getCompressAlgorithm(ByteArrayOutputStream output, int textSize) throws IOException {
-		return new LZMACompressorOutputStream(output);
+		return new LZ4BlockOutputStream(output);
 	}
 
 	@Override
