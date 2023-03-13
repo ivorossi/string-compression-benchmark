2b10980d04f71a388f19a59b72e210622054b247
ivo <rossiivomartin@gmail.com>
2023-03-13 15:22:51 -0300

    lz4

--- src/main/java/com/brightsector/stringcompressionbenchmarkivo/algorithms/LZ4ByteArrayCompressionAlgorithm.java
+++ src/main/java/com/brightsector/stringcompressionbenchmarkivo/algorithms/LZ4ByteArrayCompressionAlgorithm.java
@@ -1,29 +1,27 @@
 package com.brightsector.stringcompressionbenchmarkivo.algorithms;
 
+import java.nio.ByteBuffer;
+
 import net.jpountz.lz4.LZ4Compressor;
-import net.jpountz.lz4.LZ4Exception;
 import net.jpountz.lz4.LZ4Factory;
-import net.jpountz.lz4.LZ4SafeDecompressor;
+import net.jpountz.lz4.LZ4FastDecompressor;
 
 public class LZ4ByteArrayCompressionAlgorithm implements CompressionAlgorithm {
 	private final LZ4Factory factory = LZ4Factory.fastestInstance();
 	private final LZ4Compressor compressor = factory.highCompressor(17);
-	private final LZ4SafeDecompressor decompressor = factory.safeDecompressor();
+	private final LZ4FastDecompressor decompressor = factory.fastDecompressor();
 
 	@Override
 	public byte[] compress(byte[] data) {
-		return compressor.compress(data);
+		byte[] compress = compressor.compress(data);
+		return ByteBuffer.allocate(Integer.BYTES + compress.length).putInt(data.length).put(compress).array();
 	}
 
 	@Override
 	public byte[] uncompress(byte[] data) {
-		int lengthBuffer = data.length * 3;
-		do {
-			try {
-				return decompressor.decompress(data, lengthBuffer);
-			} catch (LZ4Exception e) {
-				lengthBuffer += lengthBuffer;
-			}
-		} while (true);
+		byte[] byteLength = new byte[Integer.BYTES];
+		System.arraycopy(data, 0, byteLength, 0, Integer.BYTES);
+		int lenght = ByteBuffer.wrap(byteLength).getInt();
+		return decompressor.decompress(data, Integer.BYTES, lenght);
 	}
 }
