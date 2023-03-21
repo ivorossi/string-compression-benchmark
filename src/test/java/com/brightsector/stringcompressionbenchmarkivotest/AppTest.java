15bef5a6ee505f8360af83ce5ce9c7c415be4c1d
ivo <rossiivomartin@gmail.com>
2023-03-21 13:50:39 -0300

    test 4 , pmd 655

--- src/test/java/com/brightsector/stringcompressionbenchmarkivotest/AppTest.java
+++ src/test/java/com/brightsector/stringcompressionbenchmarkivotest/AppTest.java
@@ -1,24 +1,24 @@
 package com.brightsector.stringcompressionbenchmarkivotest;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.NoSuchElementException;
-import java.util.Scanner;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import com.brightsector.stringcompressionbenchmarkivo.App;
+import com.brightsector.stringcompressionbenchmarkivo.algorithms.CompressionAlgorithm;
 
 public class AppTest {
 
-	private final File logFile = new File("work/logs/ivoTest.log");
-	private final String path = "src/test/resources/enwiki-test.xml.bz2";
-	private final String algorithm = "NoCompress";
+	private final File logFile = new File("work/logs/string-compression-benchmark-ivotest.log");
 
 	@BeforeEach
 	public void mainLogDelete() {
@@ -27,32 +27,28 @@ public class AppTest {
 
 	@Test
 	public void mainLogCreateTest() {
-		App.main(new String[] { path, "20", "text", algorithm });
-		assertTrue(logFile.exists());
-	}
-
-	@Test
-	public void mainLogContainsTest() throws FileNotFoundException {
-		Scanner scanner = new Scanner(logFile);
-		String line = scanner.nextLine();
-		String expected = String.format("source: %s, algorithm: %s.", path, algorithm);
-		assertEquals(expected, line.subSequence(79, line.length()));
-		line = scanner.nextLine();
-		expected = "total memory: , free memory: .";
-		assertEquals(expected, ((String) line.subSequence(79, line.length())).replaceAll("\\d+", ""));
-		line = scanner.nextLine();
-		expected = "time reading and compressing: , articles byte size: .";
-		assertEquals(expected, ((String) line.subSequence(79, line.length())).replaceAll("\\d+", ""));
-		line = scanner.nextLine();
-		expected = "time decompress: , compress byte size: .";
-		assertEquals(expected, ((String) line.subSequence(79, line.length())).replaceAll("\\d+", ""));
-		line = scanner.nextLine();
-		expected = "decompress byte size: .";
-		assertEquals(expected, ((String) line.subSequence(79, line.length())).replaceAll("\\d+", ""));
-		assertThrows(NoSuchElementException.class, () -> {
-			scanner.nextLine();
+		int amountLines = 5;
+		String path = "src/test/resources/enwiki-test.xml.bz2";
+		int metaDataLog = 79;
+		CompressionAlgorithm.ALGORITHMS.forEach((algorithm, value) -> {
+			App.main(new String[] { path, "20", "text", algorithm });
+			assertTrue(logFile.exists());
+			String[] expected = { String.format("source: %s, algorithm: %s.", path, algorithm),
+					"total memory: , free memory: .", "time reading and compressing: , articles byte size: .",
+					"time decompress: , compress byte size: .", "decompress byte size: ." };
+			try {
+				List<String> lastLines = Files.lines(Paths.get(logFile.getPath()))
+						.skip(Math.max(0, Files.lines(Paths.get(logFile.getPath())).count() - amountLines))
+						.collect(Collectors.toList());
+				assertEquals(expected[0], lastLines.get(0).subSequence(metaDataLog, lastLines.get(0).length()));
+				for (int i = 1; i < amountLines; i++) {
+					String line = (String) lastLines.get(i).subSequence(metaDataLog, lastLines.get(i).length());
+					assertEquals(expected[i], line.replaceAll("\\d+", ""));
+				}
+			} catch (IOException e) {
+				throw new IllegalArgumentException("Error reading file: " + path, e);
+			}
 		});
-		scanner.close();
 	}
 
 }
--- src/test/java/com/brightsector/stringcompressionbenchmarkivotest/compressionlimittest/CompressLimitTest.java
+++ src/test/java/com/brightsector/stringcompressionbenchmarkivotest/compressionlimittest/CompressLimitTest.java
@@ -1,18 +1,54 @@
 package com.brightsector.stringcompressionbenchmarkivotest.compressionlimittest;
 
-import java.io.File;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import com.brightsector.stringcompressionbenchmarkivo.algorithms.CompressionAlgorithm;
 import com.brightsector.stringcompressionbenchmarkivo.compressionlimit.CompressLimit;
 
 public class CompressLimitTest {
-	private final File logFile = new File("work/logs/ivoTest.log");
-	private final String path = "src/test/resources/enwiki-test.xml.bz2";
-	private final String algorithm = "NoCompress";
-	
-	@Test
-	public void mainCreate() {
-		CompressLimit.main(new String[] { path, "20", "text", algorithm, "1000" });
+
+	private final File logFile = new File("work/logs/string-compression-benchmark-ivotest.log");
+
+	@BeforeEach
+	public void mainDeleteLog() {
+		logFile.delete();
 	}
+
+	@Test
+	public void mainCreateTest() {
+		int amountPoints = 1000;
+		String path = "src/test/resources/enwiki-test.xml.bz2";
+		int metaDataLog = 106;
+		CompressionAlgorithm.ALGORITHMS.forEach((algorithm, value) -> {
+			CompressLimit.main(new String[] { path, "20", "text", algorithm, String.valueOf(amountPoints) });
+			assertTrue(logFile.exists());
+			try {
+				List<String> lastLines = Files.lines(Paths.get(logFile.getPath()))
+						.skip(Math.max(0, Files.lines(Paths.get(logFile.getPath())).count() - amountPoints))
+						.collect(Collectors.toList());
+				String expected = String.format("Algorithm: %s.", algorithm);
+				String line = (String) lastLines.get(0).subSequence(metaDataLog, lastLines.get(0).length());
+				assertEquals(expected, line);
+				expected = "length: , rate: .";
+				for (int i = 1; i < amountPoints; i++) {
+					line = (String) lastLines.get(i).subSequence(metaDataLog, lastLines.get(i).length());
+					assertEquals(expected, line.replaceAll("\\d+", ""));
+				}
+			} catch (IOException e) {
+				throw new IllegalArgumentException("Error reading file: " + path, e);
+			}
+		});
+	}
+
 }
