edfbac242398bd5483e3f8fac43431ce42b68f4a
ivo <rossiivomartin@gmail.com>
2023-03-22 12:46:42 -0300

    re-names

--- src/test/java/com/brightsector/stringcompressionbenchmarkivotest/algorithmstest/CompressionUtilTest.java
+++ src/test/java/com/brightsector/stringcompressionbenchmarkivotest/algorithmstest/CompressionUtilTest.java
@@ -7,16 +7,16 @@ import org.junit.jupiter.api.Test;
 import com.brightsector.stringcompressionbenchmarkivo.algorithms.CompressionUtil;
 
 public class CompressionUtilTest {
-	private final byte[] arrayTest = new byte[1];
+	private final byte[] testArray = new byte[1];
 
 	@Test
 	public void oneBytegetGetOriginalLengthTest() {
-		assertEquals(1, CompressionUtil.getOriginalLength(CompressionUtil.addOriginalLengthTo(arrayTest, arrayTest.length)));
+		assertEquals(1, CompressionUtil.getOriginalLength(CompressionUtil.addOriginalLengthTo(testArray, testArray.length)));
 	}
 
 	@Test
 	public void oneBytegetAddOriginalLengthTest() {
-		assertEquals(5, CompressionUtil.addOriginalLengthTo(arrayTest, arrayTest.length).length);
+		assertEquals(5, CompressionUtil.addOriginalLengthTo(testArray, testArray.length).length);
 	}
 
 }
